fb3b1f848a354945a5bc6845ad51bcf5feb5a4e0
kajari1verma <kajari@simpledatalabs.com>
2020-04-16 18:34:17 +0530

    thems color changes

--- superset/db_engine_specs/hive.py
+++ superset/db_engine_specs/hive.py
@@ -451,6 +451,4 @@ class HiveEngineSpec(PrestoEngineSpec):
         :param database: The database to get functions for
         :return: A list of function names useable in the database
         """
-        df = database.get_df("SHOW FUNCTIONS")
-        print(df)
-        return df.function.tolist()
+        return database.get_df("SHOW FUNCTIONS")["tab_name"].tolist()
