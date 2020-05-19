6d10fedc05537d9471bbcc025232203296d105bb
Mayank Kotwal <mayank@prophecy.io>
2020-05-19 16:31:45 +0530

    databricks engine

---
+++ superset/db_engine_specs/databricks.py
@@ -0,0 +1,14 @@
+from typing import List, Optional
+
+from sqlalchemy.engine.reflection import Inspector
+
+from superset.db_engine_specs.hive import HiveEngineSpec
+
+
+class DatabricksEngineSpec(HiveEngineSpec):
+    engine = "databricks+pyhive"
+
+    @classmethod
+    def get_schema_names(cls, inspector: Inspector) -> List[str]:
+        base_schema = super(DatabricksEngineSpec, cls).get_schema_names(inspector)
+        return ["global_temp"] + base_schema
