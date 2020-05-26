29030459a9e691ec400df64a98b78aff00eb1f46
Mayank Kotwal <mayank@prophecy.io>
2020-05-26 11:21:38 +0530

    invalid tables

--- superset/cli.py
+++ superset/cli.py
@@ -407,10 +407,18 @@ def update_tables_metadata():
     """Refresh Table Metadata cache"""
     from superset.connectors.sqla.models import SqlaTable
 
+    logger.info("db session: %s: %s" % (dir(db), dir(db.session)))
+    invalid_tables = []
     for table in db.session.query(SqlaTable).all():
         logger.info("Table: %s, Fetching Metadata" % (table))
-        v = table.fetch_metadata()
-        logger.info
+        logger.info("Invalid Table: %s: %s" % (table, dir(table)))
+        try:
+            v = table.fetch_metadata()
+        except Exception as e:
+            invalid_tables.append(table)
+    for invalid in invalid_tables:
+        logger.info("Invalid Table: %s: %s" % (invalid, dir(invalid)))
+
 
 @superset.command()
 @with_appcontext
