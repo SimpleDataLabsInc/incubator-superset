a375d9b1116a30b4ca5c1b88a70522d54e455a30
Mayank Kotwal <mayank@prophecy.io>
2020-06-16 15:18:43 +0530

    superset remote cli to http

--- superset/remote_cli.py
+++ superset/remote_cli.py
@@ -1,138 +1,123 @@
 import argparse
-import asyncore
+import json
 import logging
 import os
-import socket
 import subprocess
 import sys
+from wsgiref.simple_server import make_server
 
-# from watchdog.observers import Observer
-# from watchdog.events import RegexMatchingEventHandler
-
-
-
-def parse_args():
-    parser = argparse.ArgumentParser("Prophecy Superset Remote Cli")
-    parser.add_argument("-b", "--bind", default="0.0.0.0", help="Host Remote Cli Server should bind to")
-    parser.add_argument("-p", "--port", help="Port Remote Cli Server should use")
-    parser.add_argument("-l", "--location", help = "Location to watch for superset datasources")
-    return parser.parse_args()
+from pyramid.config import Configurator
+from pyramid.httpexceptions import (
+    HTTPMovedPermanently,
+    HTTPNotFound
+)
+from pyramid.response import Response
+from pyramid.view import view_config, notfound_view_config
 
 
-class Server(asyncore.dispatcher):
-
-    def __init__(self, queue_size):
-        args = parse_args()
-        HOST = os.getenv("REMOTE_CLI_HOST_STRING", args.bind)
-        LOCATION = os.getenv("SUPERSET_DATASOURCES_LOC", args.location)
-        PORT = os.getenv("REMOTE_CLI_PORT_NUMBER") or args.port
-        if PORT is None:
-            sys.exit(-1)
-        else:
-            PORT = int(PORT)
-        self.logger = logging.getLogger('SERVER')
-        asyncore.dispatcher.__init__(self)
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.set_reuse_addr()
-        self.bind((HOST, PORT))
-        self.listen(queue_size)
-        self.logger.debug('binding to {}'.format(self.socket.getsockname()))
-
-    def handle_accept(self):
-        socket, address = self.accept()
-        self.logger.debug('new connection accepted')
-        SupersetAppenderHandler(256, socket)
-
-
-class SupersetAppenderHandler(asyncore.dispatcher_with_send):
-
-    def __init__(self, max_message_length, sock = None, map = None):
-        super(SupersetAppenderHandler, self).__init__(sock, map)
-        self.debug = True
-        self.max_length = max_message_length
-        self.fmt = "%{}s".format(self.max_length)
+class HttpHandler:
+    def __init__(self, *args, **kwargs):
+        self.logger = logging.getLogger('HttpHandler')
+        (self.fac, self.request) = args
+        self.kwargs = kwargs
 
     def execute(self, cmd):
-        p = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         v = p.communicate()
         return v, p.returncode
 
-    def encodeInMultiples(self, msg):
-        length = len(msg)
-        remainder = length % self.max_length
-        div = int(length / self.max_length)
-        multiple = div * self.max_length
-        parts = msg[:multiple]
-        rest = msg[multiple:]
-        return parts + (self.fmt % rest)
+    def execute_cmd(self, cmd):
+        ((out, err), code) = self.execute(cmd)
+        full = {"code": code, "stdout": out.decode("UTF-8"), "stderr": err.decode("UTF-8")}
+        return json.dumps(full)
 
-    def handle_read(self):
-        msg = self.recv(self.max_length)
-        logging.info("Received: %s" % msg)
-        msg = msg.strip()#confjson.get('RATE', None))
-        cmd = msg.decode("utf8")
-        try:
-            ((out, err), code) = self.execute(cmd)
-            full = "OUT: " + out.decode("utf8") + ", ERROR: " + err.decode("utf8")
-        except Exception as e:
-            print(e)
-            full = "%s" % e
-        full = self.encodeInMultiples(full)
-        self.out_buffer = full.encode("utf8")
-        logging.info("Sending:  %s" % self.out_buffer)
-        if not self.out_buffer:
-            self.close()
+    @notfound_view_config(append_slash=HTTPMovedPermanently)
+    def aview(self):
+        return HTTPNotFound('not found')
+
+    @view_config(route_name="hello", request_method="GET")
+    def hello_world(self):
+        return Response('Hello World!')
+
+    @view_config(route_name = "run_cmd", request_method = "POST")
+    def run(self):
+        cmd = "superset import-datasources -s databases -p %s"
+        body = self.request.json
+        exec_cmd = cmd % body["file"]
+        resp = self.execute_cmd(exec_cmd)
+        return Response(resp)
+
+    @view_config(route_name = "update_metadata", request_method="POST")
+    def update(self):
+        cmd = "superset update-metadata -c"
+        resp = self.execute_cmd(cmd)
+        return Response(resp)
+
+
+class PyramidServer:
+    def __init__(self):
+        self.args = self.parse_args()
+        self.host = os.getenv("REMOTE_CLI_HOST_STRING", self.args.bind)
+        self.datasource_location = os.getenv("SUPERSET_DATASOURCES_LOC", self.args.location)
+        self.port = os.getenv("REMOTE_CLI_PORT_NUMBER") or self.args.port
+        if self.port is None:
+            sys.exit(-1)
+        else:
+            self.port = int(self.port)
+        self.logger = logging.getLogger('SERVER')
+        self.config = Configurator()
+        self.endpoints_added = False
+        self.app = None
+        self.server = None
+
+    def parse_args(self):
+        parser = argparse.ArgumentParser("Prophecy Superset Remote Cli")
+        parser.add_argument("-b", "--bind", default="0.0.0.0", help="Host Remote Cli Server should bind to")
+        parser.add_argument("-p", "--port", help="Port Remote Cli Server should use")
+        parser.add_argument("-l", "--location", help = "Location to watch for superset datasources")
+        return parser.parse_args()
+
+    def configure_endpoints(self):
+        self.config.add_route('hello', '/')
+        self.config.add_route("run_cmd", "/run")
+        self.config.add_route("update_metadata", "/update")
+        # self.config.add_view(self.hello_world, route_name='hello')
+        # self.config.add_view(self.run, route_name='run_cmd')
+        # self.config.add_view(self.update, route_name='update_metadata')
+        self.config.scan()
+
+    def create_app(self):
+        if not self.endpoints_added:
+            self.configure_endpoints()
+            self.endpoints_added = True
+        self.app = self.config.make_wsgi_app()
+
+    def start_server(self):
+        self.server = make_server(self.host, self.port, self.app)
+        self.logger.debug("SERVER: %s" % dir(self.server))
+        self.server.serve_forever()
+
+    def shutdown(self):
+        self.server.shutdown()
 
 
 if __name__ == "__main__":
 
-    logging.basicConfig(level=logging.DEBUG,
+    logging.basicConfig(level=logging.INFO,
                         format='[%(levelname)s] %(filename)s:%(lineno)d: %(name)s: %(message)s',
                         )
-    # with open('config.json', 'r') as jfile:
-    #     confjson = json.load(jfile)
+    server = PyramidServer()
     try:
         logging.debug('Server start')
-        server = Server(100)
-        asyncore.loop()
+        logging.debug("SERVER: %s" % dir(server))
+        server.create_app()
+        logging.debug("APP: %s" % dir(server.app))
+        server.start_server()
     except Exception as e:
-        logging.error('Something happened,\n'
+        logging.exception('Something happened,\n'
                       'if it was not a keyboard break...\n'
                       'check if address taken, '
                       'or another instance is running. Exit')
     finally:
+        server.shutdown()
         logging.debug('Goodbye')
-
-
-# class ProphecyRemoteCli(socketserver.BaseRequestHandler):
-#
-#     def handle(self):
-#         self.data = self.request.recv(50).strip()
-#         print("{} wrote: {}".format(self.client_address[0], self.data))
-#         v = "%50s" % self.data.upper()
-#         print("Written: ", v)
-#         self.request.send(v)
-#
-#
-# if __name__ == "__main__":
-#     args = parse_args()
-#     HOST = os.getenv("REMOTE_CLI_HOST_STRING", args.bind)
-#     PORT = int(os.getenv("REMOTE_CLI_PORT_NUMBER", "%s" % args.port))
-#
-#     socketserver.ThreadingTCPServer.allow_reuse_address = True
-#     server = socketserver.ThreadingTCPServer((HOST, PORT), ProphecyRemoteCli)
-#     # Create the server, binding to localhost on port 9999
-#     with server:
-#         server.serve_forever()
-#         # # Activate the server; this will keep running until you
-#         # # interrupt the program with Ctrl-C
-#         # ip, port = server.server_address
-#         # print(ip, port)
-#         #
-#         # # Start a thread with the server -- that thread will then start one
-#         # # more thread for each request
-#         # server_thread = threading.Thread(target=server.serve_forever)
-#         # # Exit the server thread when the main thread terminates
-#         # server_thread.daemon = True
-#         # server_thread.start()
-#         # print("Server loop running in thread:", server_thread.name)
