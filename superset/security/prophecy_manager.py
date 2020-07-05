1ac6fa75b4a4b3ef658403e40c703d0b2f77e1f0
Mayank Kotwal <mayank@prophecy.io>
2020-07-05 21:37:03 +0530

    fix compile error

--- superset/security/prophecy_manager.py
+++ superset/security/prophecy_manager.py
@@ -1,3 +1,5 @@
+from flask_appbuilder.security.sqla.models import User
+
 from superset import SupersetSecurityManager
 from superset.security.metagraph_client import MetagraphClient
 from superset.views.prophecy import ProphecyRemoteUserView
@@ -5,10 +7,11 @@ from superset.views.prophecy import ProphecyRemoteUserView
 
 class ProphecySecurityManager(SupersetSecurityManager):
     authremoteuserview = ProphecyRemoteUserView
+    user_model = User
 
     def __init__(self, appbuilder):
         super(ProphecySecurityManager, self).__init__(appbuilder)
-        self.client = MetagraphClient()
+        self.client = MetagraphClient(self)
 
     def find_user(self, username=None, email=None):
         return self.client.query(username)
